30512d73a2cafa62e8b4325726b2087c3b0ec35b
May Fontenot <92554133+mayfontenot@users.noreply.github.com>
2025-09-27 09:57:08 -0500

    Update Globals.hpp

--- Globals.hpp
+++ Globals.hpp
@@ -11,11 +11,11 @@ const unsigned long long CLIENT = (unsigned long long)GetModuleHandleA("client.d
 const unsigned long long ENGINE = (unsigned long long)GetModuleHandleA("engine.dll");
 
 //client addresses
-const unsigned long long forceLeft = 0xA358B8;
+const unsigned long long forceLeft = 0xA36938;
 const unsigned long long forceRight = forceLeft + 0x10;
-const unsigned long long forceJump = 0xA37870;
-const unsigned long long m_vecAbsVelocity = 0x86C110;
-const unsigned long long m_hLocalPlayer = 0x948770;
+const unsigned long long forceJump = 0xA388F0;
+const unsigned long long m_vecAbsVelocity = 0x86D110;
+const unsigned long long m_hLocalPlayer = 0x9497F0;
 
 //engine addresses
 const unsigned long long m_angAbsRotation = 0x6581FC;
@@ -31,3 +31,4 @@ const float PI = atan(1.f) * 4.f;
 //globals
 
 bool conBools[CON_B_COUNT] = {false, true, true};
+
